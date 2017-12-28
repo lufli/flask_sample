c358a20f29f466a5216f82d50f7aea89a1ac3668
Lufan Li <lufan-li@uiowa.edu>
2017-12-28 17:51:15 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+import io
+from flask import Flask, flash, request, redirect, render_template, make_response
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.secret_key = 'some_secret'
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET'])
+def upload_file():
+    return render_template('index.html')
+
+@app.route('/img', methods=['POST'])
+def show_img():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        response = make_response(file.read())
+        response.headers['Content-Type'] = 'image/jpeg'
+        return response
+    return redirect('/')
+    
+app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
---
+++ app_test.py
@@ -0,0 +1,28 @@
+from StringIO import StringIO
+import unittest
+from app import app
+
+class AppTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        self.app = app
+        self.app.config['TESTING'] = True
+        self.client = self.app.test_client()
+        
+    def tearDown(self):
+        return
+    
+    def test_upload(self):
+        f = open('./Flask.jpg', 'r')
+        data = StringIO(f.read())
+        res = self.client.post(
+            '/img',
+            content_type='multipart/form-data',
+            data={'file': (data, 'Flask.jpg')},
+            follow_redirects=True)
+        f.close()
+        f = open('./Flask.jpg', 'r')
+        assert res.data == f.read()
+        f.close()
+if __name__ == '__main__':
+    unittest.main()
